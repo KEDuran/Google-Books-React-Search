cebe02c1f3900a8fa9506e04f1fc2955b249ef02
KEDuran <krystal.e.garcia@gmail.com>
2020-09-12 17:06:29 -0500

    added styled search input component

--- client/src/components/SearchComp/index.js
+++ client/src/components/SearchComp/index.js
@@ -2,22 +2,29 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
 import Typography from "@material-ui/core/Typography";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles((theme) => ({
 	root: {
 		flexGrow: 1,
 	},
 	paper: {
+		margin: "30px 0px 0px 0px",
 		padding: theme.spacing(4),
-		textAlign: "center",
+		textAlign: "left",
 		color: theme.palette.text.secondary,
 	},
 	card: {
-		padding: "40px",
+		padding: "25px",
 		background: "#e8eaf6",
+	},
+	inputColor: {
+		background: "white",
 	},
 }));
 
@@ -30,13 +37,25 @@ export default function SearchComp() {
 				<Paper className={classes.paper}>
 					<Card className={classes.card}>
 						<CardContent>
-							<Typography variant="h5" component="h2">
-								{"(React) Google Books Search"}
-							</Typography>
-							<Typography variant="body2" component="p">
-								Search for and Save Books of Interest
+							<Typography variant="body3" component="p">
+								Search for Book
 							</Typography>
 						</CardContent>
+						<CardActions>
+							<TextField
+								id="outlined-search"
+								label="Enter name of book here"
+								type="search"
+								variant="outlined"
+								fullWidth
+								className={classes.inputColor}
+							/>
+						</CardActions>
+						<CardActions>
+							<Button variant="contained" color="primary">
+								Search
+							</Button>
+						</CardActions>
 					</Card>
 				</Paper>
 			</Grid>
