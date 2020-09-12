1385901c390329d711dd02cdfa11baa6d4547b68
KEDuran <krystal.e.garcia@gmail.com>
2020-09-12 14:55:51 -0500

    added jumbrotron component to render on page

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,15 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import JumbotronComp from "./components/Jumbotron";
 
 function App() {
 	return (
-    <Router>
-      <Navbar />
-    </Router>
-  )
- 
+		<Router>
+			<Navbar />
+			<JumbotronComp />
+		</Router>
+	);
 }
 
 export default App;
