c5171e621a18cafc911033f31854556d13279636
KEDuran <krystal.e.garcia@gmail.com>
2020-09-12 15:46:33 -0500

    started incorporating React router

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,19 @@ import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import JumbotronComp from "./components/Jumbotron";
+import SearchPage from "./pages/Search";
+import SavedPage from "./pages/Saved";
 
 function App() {
 	return (
 		<Router>
 			<Navbar />
 			<JumbotronComp />
+			<Switch>
+				<Route path="/" exact component={SearchPage} />
+				<Route path="/search" component={SearchPage} />
+				<Route path="/saved" component={SavedPage} />
+			</Switch>
 		</Router>
 	);
 }
