cb1fe86df524a5e3fa1b9ca44a3f762d3b7e3988
KEDuran <krystal.e.garcia@gmail.com>
2020-09-13 12:31:58 -0500

    added monogoose connection to server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
+const routes = require("./routes");
 const app = express();
 
 // Define middleware here
@@ -8,17 +9,18 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+	app.use(express.static("client/build"));
 }
 
-// API routes will go here
+// Add routes content
+app.use(routes);
 
-// Define any API routes before this runs
-app.get("*", (req, res) => {
-  console.log("GIVE THEM THE STATIC BUILD");
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// Connect to the Mongo DB
+mongoose.connect(
+	process.env.MONGODB_URI || "mongodb://localhost/googlebooks"
+);
 
+// Listening to start the API server
 app.listen(PORT, () => {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+	console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
