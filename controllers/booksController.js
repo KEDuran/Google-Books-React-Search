d5c72318dab04ac026cfca32afab69a317a7f3de
KEDuran <krystal.e.garcia@gmail.com>
2020-09-13 16:33:46 -0500

    added controller to find all saved books

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -1,1 +1,11 @@
-// Controller content will be included below
+const db = require("../models");
+
+// Defining methods to apply for the booksController
+module.exports = {
+	// Used to pull all saved books from Book collection
+	findAll: function (req, res) {
+		db.Book.find()
+			.then((dbModel) => res.json(dbModel))
+			.catch((err) => res.status(422).json(err));
+	},
+};
