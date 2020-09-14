18b32d1ae2679581c80e76e42930188c8c393b09
KEDuran <krystal.e.garcia@gmail.com>
2020-09-13 22:23:09 -0500

    modified search page

--- client/src/components/SearchComp/index.js
+++ client/src/components/SearchComp/index.js
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import CardActions from "@material-ui/core/CardActions";
+import CardActionArea from "@material-ui/core/CardActionArea";
 import Typography from "@material-ui/core/Typography";
+import CardMedia from "@material-ui/core/CardMedia";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
+import API from "../../utils/API";
 
 const useStyles = makeStyles((theme) => ({
 	root: {
@@ -26,15 +29,21 @@ const useStyles = makeStyles((theme) => ({
 	inputColor: {
 		background: "white",
 	},
+	media: {
+		padding: "5px",
+		width: "150px",
+		height: "200px",
+	},
 }));
 
-export default function SearchComp() {
+export default function SearchComp(props) {
 	const classes = useStyles();
 
 	return (
 		<div className={classes.root}>
 			<Grid item xs={12}>
 				<Paper className={classes.paper}>
+					{/* This if for the search input portion */}
 					<Card className={classes.card}>
 						<CardContent>
 							<Typography variant="h5" component="h2">
@@ -54,6 +63,40 @@ export default function SearchComp() {
 						<CardActions>
 							<Button variant="contained" color="primary">
 								Search
+							</Button>
+						</CardActions>
+					</Card>
+				</Paper>
+				{/* This is for the search results */}
+				<Paper className={classes.paper}>
+					<h4>{props.sectionHeader}</h4>
+
+					<Card className={classes.card}>
+						<CardActionArea>
+							<CardContent>
+								<Typography gutterBottom variant="h5" component="h2">
+									{props.title}
+								</Typography>
+
+								<Typography variant="body2" color="textSecondary" component="p">
+									{props.authors}
+								</Typography>
+								<Typography variant="body2" color="textSecondary" component="p">
+									{props.description}
+								</Typography>
+							</CardContent>
+							<CardMedia
+								className={classes.media}
+								image={require("./bookClipart.png")}
+								title="API image"
+							/>
+						</CardActionArea>
+						<CardActions>
+							<Button href={props.link} size="small" color="primary">
+								{props.btn1}
+							</Button>
+							<Button size="small" color="primary">
+								{props.btn2}
 							</Button>
 						</CardActions>
 					</Card>
--- client/src/pages/Search/index.js
+++ client/src/pages/Search/index.js
@@ -1,14 +1,12 @@
 import React from "react";
 import SearchComp from "../../components/SearchComp";
 import JumbotronComp from "../../components/Jumbotron";
-import SearchCardComp from "../../components/SearchCardComp";
 
 function SearchPage() {
 	return (
 		<div>
 			<JumbotronComp />
-			<SearchComp />
-			<SearchCardComp sectionHeader="Results" btn1="View" btn2="Save" />
+			<SearchComp btn1="View" btn2="Save"/>
 		</div>
 	);
 }
