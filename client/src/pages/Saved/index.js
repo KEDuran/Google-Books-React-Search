da76c5b41e628197cea18149057b7f01111c5c7a
KEDuran <krystal.e.garcia@gmail.com>
2020-09-13 20:17:13 -0500

    added initial get method to save page

--- client/src/pages/Saved/index.js
+++ client/src/pages/Saved/index.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import JumbotronComp from "../../components/Jumbotron";
 import CardComp from "../../components/CardComp";
 
 function SavedPage() {
+	// Setting component's initial state
+	const [books, setBooks] = useState([]);
+	const [formObject, setFormObject] = useState({});
+
+	// Load all books and store them with setBooks
+	useEffect(() => {
+		loadBooks();
+	}, []);
+
+	// Loads all books and sets them to books
+	function loadBooks() {
+		API.getBooks()
+			.then((res) => setBooks(res.data))
+			.catch((err) => console.log(err));
+	}
+	// Returns the components for the SavePage
 	return (
 		<div>
 			<JumbotronComp />
